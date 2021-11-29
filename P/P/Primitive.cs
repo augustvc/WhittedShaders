ec35659e8c1e1595806dcb6f49d5f3f6cd2a0e84
shreyesjishnu <shreyesjishnu@gmail.com>
2021-11-29 22:25:12 +0100

    PlaneImplemented

--- P/P/Primitive.cs
+++ P/P/Primitive.cs
@@ -12,15 +12,10 @@ namespace P
         public Vector3 color = new Vector3(0f, 0.5f, 0f);
         static int primitiveCount;
         int primitiveID;
-        public virtual void Intersect(Ray ray)
-        {
+        public abstract void Intersect(Ray ray);
 
-        }
+        public abstract Vector3 GetNormal(Ray ray);
 
-        public virtual Vector3 GetNormal(Ray ray)
-        {
-            return new Vector3(0.0f);
-        }
 
         public Primitive(Vector3 color)
         {
@@ -29,7 +24,7 @@ namespace P
             primitiveCount++;
         }
 
-        public int GetID ()
+        public int GetID()
         {
             return primitiveID;
         }
